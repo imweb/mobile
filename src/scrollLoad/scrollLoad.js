f9e1612d9456a44ebd4892daa996916da5dc0658
Chris Cai <caihuijigood@gmail.com>
2014-12-03 11:04:01 +0800

    Revert "Revert "添加交表""
    This reverts commit e9c6f59638b2e06dd9b6d7138b8f51d330e93ccd.

---
+++ src/scrollLoad/scrollLoad.js
@@ -0,0 +1,19 @@
+/**
+ * Created by ji on 2014/6/14.
+ */
+
+(function (root){
+
+	root.ScrollLoad = function (setting){
+
+		var opt , $el , $container;
+
+		var ctor = function (){
+
+
+		}
+
+		ctor();
+	}
+
+}(this))
